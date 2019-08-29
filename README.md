5602953c4765a0b4ea835a95a749560e8f53932a
Anuken <arnukren@gmail.com>
2019-08-28 22:44:34 -0400

    Initial upload

---
+++ README.md
@@ -0,0 +1,24 @@
+### Setup
+
+Clone this repository first.
+To edit the plugin display name and other data, take a look at `src/main.resources/plugin.json`.
+Edit the name of the project itself by going into `settings.gradle`.
+
+### Basic Usage
+
+See `src/main/java/example/ExamplePlugin.java` for some basic commands and event handlers.  
+Every main plugin class must extend `Plugin`. Make sure that `src/main/resources` points to the correct main plugin class.
+
+Please note that the plugin system is in **early alpha**, and is subject to major changes. Expect changes along the way.
+
+### Building a Jar *(without dependencies)*
+
+`gradlew jar` / `./gradlew jar`
+
+Output jar should be in `build/libs`.
+
+
+### Installing
+
+Simply place the output jar from the step above in your server's `config/plugins` directory and restart the server.
+List your currently installed plugins by running the `plugins` command.
---
+++ src/main/java/example/ExamplePlugin.java
@@ -0,0 +1,64 @@
+package example;
+
+import io.anuke.arc.*;
+import io.anuke.arc.util.*;
+import io.anuke.mindustry.*;
+import io.anuke.mindustry.content.*;
+import io.anuke.mindustry.entities.type.*;
+import io.anuke.mindustry.game.EventType.*;
+import io.anuke.mindustry.gen.*;
+import io.anuke.mindustry.plugin.Plugin;
+
+public class ExamplePlugin extends Plugin{
+
+    //register event handlers and create variables in the constructor
+    public ExamplePlugin(){
+        //listen for a block selection event
+        Events.on(BuildSelectEvent.class, event -> {
+            if(!event.breaking && event.builder.buildRequest().block == Blocks.thoriumReactor && event.builder instanceof Player){
+                //send a message to everyone saying that this player has begun building a reactor
+                Call.sendMessage("[scarlet]ALERT![] " + ((Player)event.builder).name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+            }
+        });
+    }
+
+    //register commands that run on the server
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+        handler.register("reactors", "List all thorium reactors in the map.", args -> {
+            for(int x = 0; x < Vars.world.width(); x++){
+                for(int y = 0; y < Vars.world.height(); y++){
+                    //loop through and log all found reactors
+                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor){
+                        Log.info("Reactor at {0}, {1}", x, y);
+                    }
+                }
+            }
+        });
+    }
+
+    //register commands that player can invoke in-game
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+        //register a simple reply command
+        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+        });
+
+        //register a whisper command which can be used to send other players messages
+        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
+            //find player by name
+            Player other = Vars.playerGroup.find(p -> p.name.equalsIgnoreCase(args[0]));
+
+            //give error message with scarlet-colored text if player isn't found
+            if(other == null){
+                player.sendMessage("[scarlet]No player by that name found!");
+                return;
+            }
+
+            //send the other player a message, using [lightgray] for gray text color and [] to reset color
+            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+        });
+    }
+}
