b62adb9529444069e53a6be9e57ef5eab0ef9718
Anuken <arnukren@gmail.com>
2020-12-03 13:11:08 -0500

    More examples

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -7,20 +7,37 @@ import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
 
 public class ExamplePlugin extends Plugin{
 
-    //register event handlers and create variables in the constructor
-    public ExamplePlugin(){
+    //called when game initializes
+    @Override
+    public void init(){
         //listen for a block selection event
         Events.on(BuildSelectEvent.class, event -> {
             if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
                 //player is the unit controller
-                Player player = (Player)event.builder.controller();
+                Player player = event.builder.getPlayer();
 
                 //send a message to everyone saying that this player has begun building a reactor
                 Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
             }
+        });
+
+        //add a chat filter that changes the contents of all messages
+        //in this case, all instances of "heck" are censored
+        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+
+        //add an action filter for preventing players from doing certain things
+        Vars.netServer.admins.addActionFilter(action -> {
+            //random example: prevent blast compound depositing
+            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
+                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
+                return false;
+            }
+            return true;
         });
     }
 
